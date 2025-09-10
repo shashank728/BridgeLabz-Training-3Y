aaabb50cecbbe81a2149471fa3270691de8a257d
shashank upadhyay <upadhyayshashank181@gmail.com>
2025-09-10 10:17:23 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,118 @@
+// Question 1
+// public class Assignment{
+//     public static void main(String[] args){
+//         System.out.println("Welcome to Bridgelabz!");
+//     }
+    
+// }
+
+// Question 2
+import java.util.*;
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a+b;
+//         System.out.println(sum);
+//     }
+// }
+
+// Question 3
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int cel = sc.nextInt();
+//         int fahrenhiet = (cel*(9/5))+32;
+//         System.out.println(fahrenhiet);
+        
+//     }
+// }
+
+// Question 4
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double radius = sc.nextDouble();
+//         double area = Math.PI*radius * radius;
+//         System.out.println(area);
+        
+//     }
+// }
+
+//  Question 5
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double radius = sc.nextDouble();
+//         double height = sc.nextDouble();
+//         double volume = Math.PI*(radius * radius)*height;
+//         System.out.println(volume);
+        
+//     }
+// }
+
+
+
+// Self practice Question
+// Question 6
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double principle = sc.nextDouble();
+//         double rate = sc.nextDouble();
+//         double time = sc.nextDouble();
+//         double simple_interest = (principle*rate*time)/100;
+//         System.out.println(simple_interest);
+//         sc.close();
+        
+//     }
+// }
+
+
+// Question 7
+// import java.util.*;
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int length = sc.nextInt();
+//         int width = sc.nextInt();
+//         int perimeter = 2*(length+width);
+//         System.out.println(perimeter);
+//     }
+// }
+
+// Question 8
+
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double base = sc.nextDouble();
+//         double exponent = sc.nextDouble();
+//         double result = Math.pow(base, exponent) ;
+//         System.out.println(result);
+//     }
+// }
+
+// Question 9
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double num1 = sc.nextDouble();
+//         double num2 = sc.nextDouble();
+//         double num3 = sc.nextDouble();
+//         double average = (num1+num2+num3)/3 ;
+//         System.out.println(average);
+//     }
+// }
+
+
+// Question 10 
+// public class Assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         double kilometers = sc.nextDouble();
+//         double miles = kilometers*0.621371;
+//         System.out.println(miles);
+//     }
+// }
